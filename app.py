5e1f1ed33696d01d228d39b96fe6ef60651a1b4d
JuanDavidMartinezP <jmartinezp079@inemkennedy.edu.co>
2026-05-19 20:59:38 -0500

    Se han realizado los cambios pedidos en la tarea 3. Se añadieron nuevas rutas junto sus templates y css dentro de una carpeta Static

--- app.py
+++ app.py
@@ -13,6 +13,41 @@ app = Flask(__name__)
 def inicio():
     # `render_template` busca archivos dentro de la carpeta `templates`.
     return render_template("index.html")
+
+
+
+@app.route("/ruta1")
+def route1():
+    return render_template("route1.html")
+
+
+@app.route("/ruta2")
+def route2():
+    return render_template("route2.html")
+
+
+
+@app.route("/ruta3")
+def route3():
+    return render_template("route3.html")
+
+
+@app.route("/ruta4")
+def route4():
+    return render_template("route4.html")
+
+
+@app.route("/acerca")
+def acerca():
+    return render_template("acerca.html")
+
+
+@app.route("/contacto")
+def contacto():
+    return render_template("contacto.html")
+
+
+
 # Este bloque se ejecuta solo si corremos `python app.py` desde la terminal.
 if __name__ == "__main__":
     # `debug=True` sirve en desarrollo porque reinicia el servidor
